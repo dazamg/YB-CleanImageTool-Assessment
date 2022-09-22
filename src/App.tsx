8e30861429eba86721a7f3fb33e3a0b3b9d25949
dazmag <iscope09@hotmail.com>
2022-09-22 14:26:43 -0400

    updated model

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,161 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState, useEffect, useRef} from 'react';
+import * as mobilenet from '@tensorflow-models/mobilenet';
+import "@tensorflow/tfjs";
 import './App.css';
+// import {  BarChart } from "./BarChart";
+// import {response} from './data/image_data'
+// import iti from './badimages/bad_image_1.jpeg'
+// let res = response.images;
 
+//TODO ADD the different column to the chart
+//loop the the good and bad images to set condition to add them to their groups
 function App() {
+  const[isModelLoading, setIsModelLoading] = useState(false);
+  const[model, setModel] = useState<any | null>();
+  const[imageSelected, setImageSelected] = useState<any | null>();
+  const[results, setResults] = useState<any[]>([]);
+  // const [chartData, setChartData] = useState({});
+  const [goodImage, setGoodImage] = useState<any[]>([]);
+  const [badImage, setBadImage] = useState<any[]>([]);
+
+  const imageRef = useRef<any | null>();
+  const fileInputRef = useRef<any | null>();
+
+  // useEffect(() => {
+  //   setChartData({
+  //     labels: response.images.map((result) => result.id),
+  //     datasets: [
+  //       {
+  //         label: "Number of Images",
+  //         data: response.images.map((results) => results.image1),
+  //         backgroundColor: [
+  //           "#ffbb11",
+  //           "#ecf0f1",
+  //           "#50AF95",
+  //           "#f3ba2f",
+  //           "#2a71d0"
+  //         ]
+  //       }
+  //     ]
+  //   });
+  // },[])
+  const loadModel = async () => {
+    setIsModelLoading(true)
+    try {
+      const model = await mobilenet.load()
+      setModel(model)
+      setIsModelLoading(false)
+    } catch(error){
+      console.log(error)
+      setIsModelLoading(false)
+    }
+  }
+  
+  const imageUpload = (e:any) => {
+    const {files} = e.target;
+    if(files.length > 0){
+      const url = URL.createObjectURL(files[0])
+      setImageSelected(url)
+    }else {
+      setImageSelected(null)
+    }
+  }
+
+  const analize = async () => {
+    const results = await model.classify(imageRef.current)
+    setResults(results)
+    console.log("@@@@@@@@",results)
+    if(results.probability > 0.3){
+      setBadImage(results)
+    }else{
+      setGoodImage(results)
+    }
+  }
+
+  const triggerUpload = () => {
+    fileInputRef.current.click()
+  }
+  useEffect(() => {
+    loadModel()
+  },[])
+
+
+  if(isModelLoading){
+    return <h2>Model Loading...</h2>
+  }
+
+  console.log("@@@@@@@@",results)
+  console.log("bad image",badImage)
+  console.log("good image",goodImage)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+        <h1 className='App-header'>Yembo Data Visualization Tool</h1>
+        <div className='inputHolder'>
+            <input type='file' accept='image/*'  className='uploadInput' onChange={imageUpload} ref={fileInputRef} />
+            <button className='uploadImage' onClick={triggerUpload}>Upload Image</button>
+
+        </div>
+        <div className="mainContainer">
+            <div className="mainContent">
+                <div className="imageContainer">
+                    {imageSelected && <img src={imageSelected} alt="Upload Preview" crossOrigin="anonymous" ref={imageRef}/>}
+                </div>
+                {results.length > 0 && <div className='resultsHolder'>
+                    {results.map((result, index) => {  
+                        return (
+                            <div className='result' key={result.className}>
+                                <span className='name'>{result.className}</span>
+                                <span className='confidence'>Confidence level: {(result.probability * 100).toFixed(2)}% {index === 0 && <span className='bestGuess'>Best Guess</span>}</span>
+                            </div>
+                        )
+                    })}
+                </div>}
+            </div>
+            {imageSelected && <button className='button' onClick={analize}>Analize Image</button>}
+        </div>
+        <div className='image_group'>
+        <ul><h1>Good Images</h1>
+          <li> 
+              {goodImage.map((result, index) => {
+                  return (
+                      <div className='result' key={result.className}>
+                          <span className='name'>{result.className}</span>    
+                      </div>
+                  )
+              })}
+                
+            </li>
+        </ul>
+        <ul><h1>Bad Images</h1>
+          <li> 
+              {badImage.map((result, index) => {
+                  return (
+                      <div className='result' key={result.className}>
+                          <span className='name'>{result.className}</span>
+                          <span className='confidence'>Confidence level: {(result.probability * 100).toFixed(2)}% {index === 0 && <span className='bestGuess'>Best Guess</span>}</span>
+                      </div>
+                  )
+              })}
+          </li>
+          </ul>
+       
+ 
+        </div>
+        {/* {history.length > 0 && <div className="recentPredictions">
+            <h2>Recent Images</h2>
+            <div className="recentImages">
+                {history.map((image, index) => {
+                    return (
+                        <div className="recentPrediction" key={`${image}${index}`}>
+                            <img src={image} alt='Recent Prediction' onClick={() => setImageSelected(image)} />
+                        </div>
+                    )
+                })}
+            </div>
+        </div>} */}
+    </>
+);
 }
 
 export default App;
+
---
+++ src/data/image_data.js
@@ -0,0 +1,33 @@
+export const response = {
+    "images": [
+        {
+            id: 1,
+            image1: "../badimages/bad_image_1.jpeg"
+        },
+        {
+            id: 2,
+            image2: "../badimages/bad_image_2.jpeg"
+        },
+        {
+            id: 3,
+            image3: "../badimages/bad_image_3.jpeg"
+        },
+        {
+            id: 4,
+            image4: "../badimages/bad_image_4.jpeg"
+        },
+        {
+            id: 5,
+            image5: "../goodimages/good_image_1.jpeg"
+        },
+        {
+            id: 6,
+            image6: "../goodimages/good_image_2.jpeg"
+        },
+        {
+            id: 7,
+            image7: "../goodimages/good_image_3.jpeg"
+        }
+    
+    ]
+}
